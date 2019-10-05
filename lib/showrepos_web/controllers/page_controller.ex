91744ac2e7e1f7bee95043bfcbc3403d6aff9a60
Morgan Espitia <olliedeops@gmail.com>
2019-10-05 12:49:29 -0700

    build graphql query, add auth token via .env

--- lib/showrepos_web/controllers/page_controller.ex
+++ lib/showrepos_web/controllers/page_controller.ex
@@ -2,10 +2,42 @@ defmodule ShowreposWeb.PageController do
   use ShowreposWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html", token: get_csrf_token())
+    render(conn, "index.html")
   end
 
   def getRepos(conn, %{"username" => username}) do
+    body = Jason.encode!(%{
+      "query": "query($login: String!) {
+        user(login: $login) {
+          watching(first: 100) {
+            edges {
+              node {
+                name
+                url
+              }
+            }
+          } 
+          repositories(first: 100){
+            edges {
+              node {
+                name
+                url
+              }
+            }
+          }
+        }
+      }",
+      "variables": "{
+        \"login\": \"#{username}\"
+      }"
+    })
+    headers = %{"Authorization": "Bearer #{System.get_env("GITHUB_ACCESS_TOKEN")}"}
+
+    response = HTTPoison.post!("https://api.github.com/graphql", body, headers)
+    responseMap = Jason.decode!(response.body)
+
+    IO.inspect response.body
+    
     text conn, "#{username}"
   end
 end
